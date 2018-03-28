80947e3ea93fe810dcd23a686e94a50fca079b23
kol4k <rafinyadi@gmail.com>
2018-03-28 17:25:02 +0700

    Upload and Process Import to MySQL

--- admin/upload.php
+++ admin/upload.php
@@ -1,16 +1,42 @@
 <?php
-//input poto
-$photo1 = $_FILES['gbr1']['name'];
-$extensionList = array("jpeg", "jpg", "png", "JPG", "JPEG", "PNG");
-$dir = "gambar_news/";
+include("../config/database.php");
+include("../Classes/PHPExcel/IOFactory.php");
+
+$file = $_FILES['import']['name'];
+$extensionList = array("xls","xlsx","XLS","XLSX");
 $pecah1 = explode(".", $photo1);
 $ekstensi1 = $pecah1[1];
-if (in_array($ekstensi1,$extensionList))
-{
-	$tmp1 = $_FILES['gbr1']['tmp_name'];
-	$nama_baru1 = $id_news.'1'.".";
+
+if (in_array($ekstensi1,$extensionList)) {
+	$tmp1 = $_FILES['import']['tmp_name'];
+	$nama_baru1 = $file.'1'.".";
 	$nama_baru1 = $nama_baru1.$ekstensi1;
 	$upload = move_uploaded_file($tmp1,$dir.$nama_baru1);
- } else {echo "hanya diperbolehkan file ekstension jpeg,jpg,png "; } 
- if (!empty($judul))
+		
+	// Create new PHPExcel object
+	$objPHPExcel = PHPExcel_IOFactory::load($upload);
+	
+	$dataArr = array();
+	
+	foreach ($objPHPExcel->getWorksheetIterator() as $worksheet) {
+		$worksheetTitle     = $worksheet->getTitle();
+		$highestRow         = $worksheet->getHighestRow(); // e.g. 10
+		$highestColumn      = $worksheet->getHighestColumn(); // e.g 'F'
+		$highestColumnIndex = PHPExcel_Cell::columnIndexFromString($highestColumn);
+		
+		for ($row = 1; $row <= $highestRow; ++ $row) {
+			for ($col = 0; $col < $highestColumnIndex; ++ $col) {
+				$cell = $worksheet->getCellByColumnAndRow($col, $row);
+				$val = $cell->getValue();
+				$dataArr[$row][$col] = $val;
+			}
+		}
+	}
+	unset($dataArr[1]); // since in our example the first row is the header and not the actual data
+	foreach($dataArr as $val){
+		$query = $db->query("INSERT INTO mahasiswa SET fname = '" . $db->escape($val['1']) . "', lname = '" . $db->escape($val['2']) . "', email = '" . $db->escape($val['3']) . "', phone = '" . $db->escape($val['4']) . "', company = '" . $db->escape($val['5']) . "'");
+	}
+} else {
+	echo "hanya diperbolehkan file ekstension jpeg,jpg,png "; 
+} 
 ?>
